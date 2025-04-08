9a214902e6878d6e17aba7b4aa059fff5ea891b2
Kailie Field <kailie.field@gmail.com>
2025-04-07 22:50:30 -0400

    feat: build employee list table with dynamic GraphQL data

--- src/app/pages/employee-list/employee-list.component.ts
+++ src/app/pages/employee-list/employee-list.component.ts
@@ -1,11 +1,64 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { gql, Apollo } from 'apollo-angular';
+
+
+
+const GET_ALL_EMPLOYEES = gql`
+
+  getAllEmployees {
+
+    _id
+    first_name
+    last_name
+    designation
+    department
+
+  }
+`;
 
 @Component({
+
   selector: 'app-employee-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './employee-list.component.html',
-  styleUrl: './employee-list.component.css'
+  styleUrls: ['./employee-list.component.css']
+
 })
+
+
 export class EmployeeListComponent {
+
+  employees: any[] = [];
+  loading: boolean = true;
+  error: string = '';
+
+  constructor( private apollo: Apollo ) { }
+
+  ngOnInit(){
+
+    this.apollo.watchQuery<any>({
+
+      query: GET_ALL_EMPLOYEES
+    }).valueChanges.subscribe({
+
+      next: (result) => {
+
+        this.employees = result.data.getAllEmployees;
+        this.loading = false;
+
+      },
+
+      error: (error) => {
+        
+        this.error = error.message;
+        this.loading = false;
+
+      }
+    });
+  }
 
 }
