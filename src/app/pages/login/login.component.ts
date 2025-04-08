8a16416bb1f835a13bd07b2a123641883ffbc2f8
Kailie Field <kailie.field@gmail.com>
2025-04-07 21:41:30 -0400

    important to remember that you constructor must not have returns at the service class level

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
+
   selector: 'app-login',
-  imports: [],
+  imports: [], //<----- ** will have to add imports 
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
+
 })
+
+
 export class LoginComponent {
 
+  username: string = '';
+  password: string = '';
+
+  constructor( private authService: AuthService) {}
+
+    onLogin() {
+      this.authService.authenticateUser(this.username, this.password).subscribe({
+
+        next: (result) => {
+          console.log('[ USER LOGGED IN ]', result);
+
+          //---- saving token to local storage: 
+          const token = result.data.login;
+          localStorage.setItem('token', token);
+
+          //**** mental note: nav to go here
+
+        },
+
+        error: (error) => {
+          console.error('[FAILED TO LOGIN]', error.message);
+        }
+      });
+    }
 }
