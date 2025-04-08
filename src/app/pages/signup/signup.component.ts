97b9332ba006f315720727559ea57d9005495c13
Kailie Field <kailie.field@gmail.com>
2025-04-07 22:37:08 -0400

    feat: implement SignupComponent with Apollo GraphQL registration flow

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
+
   selector: 'app-signup',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrls: ['./signup.component.css']
+
 })
+
+
 export class SignupComponent {
+
+  email: string = '';
+  username: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService) { }
+
+  registerUser() {
+    this.authService.registerUser(this.username, this.email, this.password).subscribe({
+
+      next: (result) => {
+        console.log('[ SUCCESSFUL SIGNUP ]', result);
+      },
+
+      error: (error) => {
+        console.error('[ SIGNUP FAILED ]', error.message);
+      }
+    });
+  }
+
+
 
 }
